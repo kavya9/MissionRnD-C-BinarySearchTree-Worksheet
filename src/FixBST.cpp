d4a29826199e3719d8d5e410f84ff20330194961
kavya9 <kavya9@missionrnd.com>
2016-05-22 00:13:55 +0530

    Submit Commit

--- src/FixBST.cpp
+++ src/FixBST.cpp
@@ -31,7 +31,74 @@ struct node{
 	int data;
 	struct node *right;
 };
+/*void findMisplacedNodes(struct node*root,struct node*previous,struct node*firstElement,struct node*secondElement)
+{
+	if (root == NULL) return;
+	findMisplacedNodes(root->left, previous, firstElement, secondElement);
+	if (previous != NULL)
+	{
+		if (previous->data > root->data)
+		{
+			if (firstElement == NULL)
+			{
+				firstElement = previous;
+			}
+			secondElement = root;
+		}
+	}
+	previous = root;
+	findMisplacedNodes(root->right, previous, firstElement, secondElement);
 
+}*/
+int noOfNodes1(struct node* root)
+{
+	if (root == NULL)return 0;
+	return 1 + noOfNodes1(root->left) + noOfNodes1(root->right);
+}
+void inOrderTraversal1(struct node *root, struct node **nodeArr, int *index)
+{
+	if (root == NULL || nodeArr == NULL) return;
+	inOrderTraversal1(root->left, nodeArr, index);
+	nodeArr[*index] = root;
+	(*index) = (*index) + 1;
+	inOrderTraversal1(root->right, nodeArr, index);
+
+}
 void fix_bst(struct node *root){
+	/*struct node *previous=NULL, *firstElement=NULL, *secondElement=NULL;
+	if (root != NULL)
+	{
+		findMisplacedNodes(root, previous, firstElement, secondElement);
+		int temp = firstElement->data;
+		firstElement->data = secondElement->data;
+		secondElement->data = temp;
+	}*/
+	if (root != NULL)
+	{
+		int nodes = noOfNodes1(root),i;
+		struct node **nodeArr = (struct node**)malloc(nodes*sizeof(struct node*));
+		int index = 0;
+		inOrderTraversal1(root, nodeArr, &index);
+		struct node *first, *second;
+		first = NULL; second = NULL;
+		int flag = 0;
+		for (i = 1; i < nodes; i++)
+		{
+			if (nodeArr[i - 1]->data > nodeArr[i]->data)
+			{
+				if (first == NULL)
+				{
+					first = nodeArr[i - 1];
+				}
+				second = nodeArr[i];
+			}
+		}
+		
+		int num;
+		num = first->data;
+		first->data = second->data;
+		second->data = num;
 
+
+	}
 }
--- src/MainBST.cpp
+++ src/MainBST.cpp
@@ -17,6 +17,16 @@ struct node{
 	int data;
 	struct node *right;
 };
+void inOrderTraversal2(struct node *root, int *arr, int *index)
+{
+	if (root == NULL || arr == NULL) return;
+	inOrderTraversal2(root->left, arr, index);
+	arr[*index] = root->data;
+	printf("%d\n", root->data);
+	(*index) = (*index) + 1;
+	inOrderTraversal2(root->right, arr, index);
+
+}
 
 struct node *newNode(int key)
 {
@@ -26,8 +36,102 @@ struct node *newNode(int key)
 	temp->right = NULL;
 	return temp;
 }
+struct node * add_node_spec(struct node *root, int data){
+	if (root == NULL) return newNode(data);
+
+	if (data < root->data)
+		root->left = add_node_spec(root->left, data);
+	else if (data > root->data)
+		root->right = add_node_spec(root->right, data);
+
+	return root;
+}
+void swap_nodes(struct node *a, struct node *b){
+	int temp = a->data;
+	a->data = b->data;
+	b->data = temp;
+}
+int noOfNodes11(struct node* root)
+{
+	if (root == NULL)return 0;
+	return 1 + noOfNodes11(root->left) + noOfNodes11(root->right);
+}
+void inOrderTraversal11(struct node *root, struct node *nodeArr, int *index)
+{
+	if (root == NULL || nodeArr == NULL) return;
+	inOrderTraversal11(root->left, nodeArr, index);
+	nodeArr[*index] = *root;
+	//printf("%d\n", root->data);
+	(*index) = (*index) + 1;
+	inOrderTraversal11(root->right, nodeArr, index);
+
+}
+void fix_bst1(struct node *root){
+	if (root != NULL)
+	{
+		int nodes = noOfNodes11(root);
+		struct node *nodeArr = (struct node*)malloc(nodes*sizeof(struct node));
+		int index = 0, i;
+		inOrderTraversal11(root, nodeArr, &index);
+
+		
+		int  index1 = 0;
+		int *arr = (int *)malloc(nodes*sizeof(int));
+		inOrderTraversal2(root, arr, &index1);
+
+
+		/*for (i = 0; i < nodes; i++)
+			printf("%d\n", nodeArr[i].data);*/
+		struct node *first = (struct node*)malloc(sizeof(struct node));
+		struct node *second = (struct node *)malloc(sizeof(struct node));
+		first->data = NULL; second->data = NULL;
+		for (i = 1; i < nodes; i++)
+		{
+			if (nodeArr[i - 1].data > nodeArr[i].data)
+			{
+				if (first->data == NULL)
+				{
+					first->left = nodeArr[i - 1].left;
+					first->right = nodeArr[i - 1].right;
+					first->data = nodeArr[i - 1].data;
+				}
+				second->left = nodeArr[i].left;
+				second->right = nodeArr[i].right;
+				second->data = nodeArr[i].data;
+			}
+		}
+		//printf("%d\n%d\n", first.data, second.data);
+		struct node *temp = first;
+		first = second;
+		second = temp;
+	}
+}
+
 
 int main(){
 
 	//Use it for testing ,Creating BST etc
+	int nums[10] = { 2, 1, 3 };
+	struct node *root = NULL;
+	for (int i = 0; i < 3; i++){
+		root = add_node_spec(root, nums[i]);
+	}
+	struct node *swaplist[2];
+	int originaldata[2];
+	swaplist[0] = root->left;
+	swaplist[1] = root->right;
+	originaldata[0] = swaplist[0]->data;
+	originaldata[1] = swaplist[1]->data;
+
+	swap_nodes(swaplist[0], swaplist[1]);
+	fix_bst1(root);
+	int nodes = noOfNodes11(root);
+	struct node *nodeArr = (struct node*)malloc(nodes*sizeof(struct node));
+	int index = 0, i;
+	inOrderTraversal11(root, nodeArr, &index);
+	for (i = 0; i < nodes; i++)
+		printf("%d\n", nodeArr[i].data);
+	getchar();
+	return 0;
 }
+
